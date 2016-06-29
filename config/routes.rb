c701aa2499955084a09ca80a9284d1d6178c61d7
Panocracy <info@pancracy.org.uk>
2016-06-29 06:20:36 +0100

    rails_apps_composer: add pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+
+    root to: "users#index"
+  end
+
+  root to: 'visitors#index'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
